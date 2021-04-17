943d0f07073a2e799c327089f45f60c5cbd2e26b
Ishtiak Ahmed <iahmed1606@gmail.com>
2021-04-17 14:34:16 +0600

    try to solve heroku

--- server.js
+++ server.js
@@ -1,18 +1,18 @@
 const express = require('express')
 const cors = require('cors')
+const MongoClient = require('mongodb').MongoClient;
 require('dotenv').config()
+const port = 1929
 
 const app = express()
 app.use(cors())
 app.use(express.json())
 
-const port = 1929
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
-const MongoClient = require('mongodb').MongoClient;
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xzynl.mongodb.net/${process.env.DATABASE}?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
