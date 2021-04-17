1d16b3451090f58577e529b168a795f5dc50c731
Ishtiak Ahmed <iahmed1606@gmail.com>
2021-04-18 02:29:05 +0600

    admin auth added

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
 const express = require('express')
 const cors = require('cors')
 const MongoClient = require('mongodb').MongoClient;
+const fileupload = require('express-fileupload')
 require('dotenv').config()
 const port = 1929
 
 const app = express()
 app.use(cors())
 app.use(express.json())
+app.use(express.static('serviceimg'))
+app.use(fileupload())
+// app.use(fileUpload({
+//     useTempFiles: true,
+//     tempFileDir: '/tmp/'
+// }));
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
@@ -19,6 +26,8 @@ client.connect(err => {
     const serviceCollection = client.db(`${process.env.DATABASE}`).collection("services");
     const orderCollection = client.db(`${process.env.DATABASE}`).collection("orders");
     const reviewCollection = client.db(`${process.env.DATABASE}`).collection("reviews");
+    const adminsCollection = client.db(`${process.env.DATABASE}`).collection("admins");
+
     // Add Booking
     app.post('/addBooking', (req, res) => {
         orderCollection.insertOne(req.body)
@@ -31,9 +40,18 @@ client.connect(err => {
     })
     // Get Orderlist of User
     app.post('/orderList', (req, res) => {
-        console.log(req.body);
-        orderCollection.find({ 'booking.email': req.body.email })
-            .toArray((err, docs) => res.send(docs))
+        const email = req.body.email
+        adminsCollection.find({ email: email })
+            .toArray((err, docs) => {
+                if (docs.length > 0) {
+                    orderCollection.find()
+                        .toArray((err, docs) => res.send(docs))
+                } else {
+                    orderCollection.find({ 'booking.email': email })
+                        .toArray((err, docs) => res.send(docs))
+                }
+            })
+
     })
     // Get Orderlist of User
     app.post('/fullOrderList', (req, res) => {
@@ -41,10 +59,43 @@ client.connect(err => {
         orderCollection.find({ 'booking.email': req.body.email })
             .toArray((err, docs) => res.send(docs))
     })
+    // Add A Service
+    app.post('/addAService', (req, res) => {
+        console.log('service is adding..')
+        const file = (req.files.file)
+        const sevice = req.body.service
+        const description = req.body.description
+        const newImg = file.data;
+        const encImg = newImg.toString('base64');
+
+        const image = {
+            contentType: file.mimetype,
+            size: file.size,
+            img: Buffer.from(encImg, 'base64')
+        };
+        serviceCollection.insertOne({ sevice, description, image })
+            .then(result => {
+                res.send(result.insertedCount > 0);
+            })
+    })
     // Get Reviews
     app.get('/reviews', (req, res) => {
         console.log('review req recieved')
         reviewCollection.find().toArray((err, docs) => res.send(docs))
+    })
+    // Get Services
+    app.get('/services', (req, res) => {
+        serviceCollection.find()
+            .toArray((err, docs) => res.send(docs))
+    })
+
+    // Check Admin
+    app.post('/checkAdmin', (req, res) => {
+        const email = req.body.email
+        adminsCollection.find({ email: email })
+            .toArray((err, docs) => {
+                res.send(docs.length > 0)
+            })
     })
 });
 app.listen(process.env.PORT || port)
